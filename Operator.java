f62a01ce7aeb2c36243dd28f307e73f7fafd6207
pratiksha1181 <sanikaure961@gmail.com>
2025-12-20 19:22:20 +0530

    operator file

---
+++ Operator.java
@@ -0,0 +1,30 @@
+package Practice;
+
+public class Operator {
+
+	public static void main(String[] args) {
+		
+			int a=20;
+			int b=10;
+			int c=a+b;
+			int d=a-b;
+			int e=a*b;
+			int f=a/b;
+			
+//			int 2ndNumber = 5;  // Cannot start with a digit
+//			int my var = 10;    // Cannot contain spaces
+//			int int = 20;       // Cannot use reserved keywords
+//			
+			
+			System.out.println(2+4*5);
+			System.out.println((2+4)*5); //solve bracket first
+			
+			System.out.println(c);
+			System.out.println(d);
+			System.out.println(e);
+			System.out.println(f);
+
+
+	}
+
+}
